46e433a97241d6a98290b84e6a67eb8ae63b3dc3
Rich Xu <richardxu100@gmail.com>
2016-08-22 22:48:13 -0400

    Routing Path wiring is set up

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,11 +3,10 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import AppContainer from '../containers/AppContainer';
 
 const routes = (
-	<Router>
-		<Route history={browserHistory}>
-			<IndexRoute component={AppContainer} />
+	<Router history={browserHistory}>
+		<Route path="/" component={AppContainer}>
 		</Route>
 	</Router>
-) 
+)
 
 export default routes;
