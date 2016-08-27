699be387cb1482f265a9f39c7d80a6a72d6f386c
Rich Xu <richardxu100@gmail.com>
2016-08-27 16:26:49 -0400

    Added form validation to modalForm

--- app/containers/ModalForm.js
+++ app/containers/ModalForm.js
@@ -1,7 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
-import TextField from 'material-ui/TextField';
+import { Form } from 'formsy-react';
+import FormsyText from 'formsy-material-ui/lib/FormsyText';
 import styles from '../styles/styles';
 
 export default class ModalForm extends Component {
@@ -20,42 +21,43 @@ export default class ModalForm extends Component {
       this.state = {
         name: props.name,
         ingredients: props.ingredients.join(', '),
-        imageURL: props.imageURL
+        imageURL: props.imageURL,
+        canSubmit: false
       }
-    }
+    } else this.state = { canSubmit: false }
   }
+
+  enableButton = () => this.setState({canSubmit: true});
+  disableButton = () => this.setState({canSubmit: false});
 
   handleNameChange = (e) => this.setState({name: e.target.value});
   handleIngredientsChange = (e) => this.setState({ingredients: e.target.value});
   handleImageURLChange = (e) => this.setState({imageURL: e.target.value})
 
   handleSubmitRecipe = () => {
-    const recipeName = this.recipeName.input.value;
-    const ingredients = this.ingredients.input.value;
-    const imageURL = this.imageURL.input.value;
-    console.log(recipeName, ingredients, imageURL);
+    const recipeName = this._recipeName.state.value;
+    const ingredients = this._ingredients.state.value;
+    const imageURL = this._imageURL.state.value;
     if (this.props.id) {
       const { onSubmit, id } = this.props;
       onSubmit(id, recipeName, ingredients, imageURL);
     }
-    else {
-      this.props.onSubmit(recipeName, ingredients, imageURL);
-    }
+    else this.props.onSubmit(recipeName, ingredients, imageURL);
   }
 
   render() {
     const { isOpen, toggleOpen, id } = this.props;
-    if (id) {
-      const { name, ingredients, imageURL } = this.state;
-    }
+    const { name, ingredients, imageURL } = this.state;
     const actions = [
       <FlatButton
         label="Cancel"
         onTouchTap={toggleOpen.bind(this, id)} />,
       <FlatButton
+        type="submit"
         label={this.props.id ? 'Edit' : 'Add'}
         primary={true}
-        onTouchTap={this.handleSubmitRecipe.bind(this, id)} />,
+        onTouchTap={this.handleSubmitRecipe.bind(this, id)}
+        disabled={!this.state.canSubmit} />
     ];
     return (
       <Dialog
@@ -63,25 +65,35 @@ export default class ModalForm extends Component {
         actions={actions}
         modal={true}
         open={isOpen}>
-        <TextField
-          hintText="Recipe Name"
-          ref={r => this.recipeName = r}
-          value = {id ? this.state.name : undefined}
-          onChange = {id ? this.handleNameChange : undefined} /> <br />
-        <TextField
-          style={styles.textField}
-          hintText="Ingredients: separated by a comma, for, each, ingredient"
-          fullWidth={true}
-          ref={r => this.ingredients = r}
-          value={id ? this.state.ingredients : undefined}
-          onChange={id ? this.handleIngredientsChange : undefined} /> 
-        <TextField
-          style={styles.textField}
-          hintText="Optional Image URL"
-          fullWidth={true}
-          ref={r => this.imageURL = r}
-          value={id ? this.state.imageURL : undefined}
-          onChange={id ? this.handleImageURLChange : undefined} />
+        <Form
+          onValidSubmit={this.handleRegister}
+          onValid={this.enableButton}
+          onInvalid={this.disableButton}>
+          <FormsyText
+            required
+            name="recipeName"
+            hintText="Recipe Name"
+            ref={r => this._recipeName = r}
+            value = {id ? this.state.name : undefined}
+            onChange = {id ? this.handleNameChange : undefined} /> <br />
+          <FormsyText
+            required
+            name="ingredients"
+            style={styles.textField}
+            hintText="Ingredients: separated by a comma, for, each, ingredient"
+            fullWidth={true}
+            ref={r => this._ingredients = r}
+            value={id ? this.state.ingredients : undefined}
+            onChange={id ? this.handleIngredientsChange : undefined} />
+          <FormsyText
+            name="imageURL"
+            style={styles.textField}
+            hintText="Optional Image URL"
+            fullWidth={true}
+            ref={r => this._imageURL = r}
+            value={id ? this.state.imageURL : undefined}
+            onChange={id ? this.handleImageURLChange : undefined} />
+          </Form>
         </Dialog>
     )
   }
